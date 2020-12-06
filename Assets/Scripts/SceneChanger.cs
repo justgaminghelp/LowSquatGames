4d80a548edce14f3ff34574a7eaadee7cfb6a43d
justgaminghelp <justgaminghelp@mail.ru>
2020-12-06 14:05:53 +0300

    Add main background and start and setting button

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void Scene_Changer(string name)
+    {
+        SceneManager.LoadScene(name,LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
